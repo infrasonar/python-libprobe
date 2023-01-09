604d8da1f43b2538619e928e05153332a1051e0c
Jeroen van der Heijden <jeroen@cesbit.com>
2023-01-09 10:07:37 +0100

    _use may be empty string

--- libprobe/config.py
+++ libprobe/config.py
@@ -51,7 +51,9 @@ def decrypt(layer, fernet):
 
 def get_config(conf: dict, probe_name: str, asset_id: int, use: Optional[str]):
     probe = conf.get(probe_name)
-    if isinstance(use, str):
+    if use:
+        # use might both be None or an empty string, depending if the _use
+        # option is available for the probe; both must be ignored
         probe = conf.get(use, probe)
 
     if not isinstance(probe, dict):
